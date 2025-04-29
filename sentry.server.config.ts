8ba129482cb1583696a0f896abccad6a68c56525
PhamTinh <tinh1235a@gmail.com>
2025-04-29 21:01:54 +0900

    updata sentry 2

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
+  dsn: "https://8619ae967404e698833519d38de0a7a6@o4509111657693185.ingest.us.sentry.io/4509234406359040",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
